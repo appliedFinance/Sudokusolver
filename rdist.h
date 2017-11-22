b7501257855ed6f07198f4bd48a1466968efc30a
appliedFinance <atlusr@yahoo.com>
2017-11-22 14:08:36 -0500

    adding distribution header.

---
+++ rdist.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+double gauss(double, double);
+double expo(double);
+unsigned int poisson(double);
+double alea();
+
+double uniform(double,double);
+int nUniform(int, int);
+
+
